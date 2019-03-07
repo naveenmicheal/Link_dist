be25881996ef5f0136536d83895cae7d0cc02205
Naveen Micheal <naveenmicheal@protonmail.com>
2019-03-07 22:56:54 +0530

    Funcional

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 from flask import Flask, url_for, render_template, request
-
+import requests as rq
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
 	if request.method =='POST':
-		return 'ok'
+		i_url = request.form.get('url')
+		rs = rq.get(i_url)
+		d_url = rs.url;
+		return render_template('home.html', data=d_url)
 	else:	
 		return render_template('home.html')
-	
+# @app.route('/url/<string:iurl>')
+# def url(iurl):
+# 	# if (iurl[0:7]) !='http://':
+# 	# 	iurl ='http://'+iurl
+# 	iurl = 'http://'+iurl
+# 	print(iurl)	
+# 	rs = rq.get(iurl)
+# 	d_url = rs.url;
+# 	return 	d_url
+
 @app.route('/about')
 def about():
 	return '<h3>Hello World</h3>'
