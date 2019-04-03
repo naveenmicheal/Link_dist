a630aa4498de47a8fc69a950b0d0998bff1a92bb
Naveen Micheal <naveenmicheal@protonmail.com>
2019-04-03 23:22:31 +0530

    Finish Initial Front END design

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, url_for, render_template, request
 import requests as rq
+from bs4 import BeautifulSoup
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -7,8 +9,27 @@ def home():
 	if request.method =='POST':
 		i_url = request.form.get('url')
 		rs = rq.get(i_url)
-		d_url = rs.url;
-		return render_template('home.html', data=d_url)
+		d_url = rs.url
+		soup = BeautifulSoup(rs.content,'html.parser')
+		soup.prettify()
+		data = {
+		'title':soup.title.string,
+		'google_url':("""https://transparencyreport.google.com
+			/safe-browsing/search?url={}""".format(d_url)),
+		'norton' :("https://safeweb.norton.com/report/show?url={}".format(d_url)),
+		'wot' :("https://www.mywot.com/en/scorecard/{}".format(d_url))
+		}
+
+		# title = soup.title.string
+		# google_url = ("""https://transparencyreport.google.com
+		# 	/safe-browsing/search?url={}""".format(d_url))
+		# nortan =("https://safeweb.norton.com/report/show?url={}".format(d_url))
+		# wot =("https://www.mywot.com/en/scorecard/{}".format(d_url))
+		print(" \n\n\n\n ############ DATA ################# \n\n\n")
+		# print(d_url)
+		# print(title)
+		
+		return render_template('home.html', data=data)
 	else:	
 		return render_template('home.html')
 # @app.route('/url/<string:iurl>')
